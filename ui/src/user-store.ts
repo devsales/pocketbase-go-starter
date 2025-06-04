f820307084e77c5a65d285b4ae28e8648c8eb9fb
Pavel Nikolov <pavel.nikolov@outlook.com>
2025-06-05 00:49:40 +0200

    prettier formatting

--- ui/src/user-store.ts
+++ ui/src/user-store.ts
@@ -1,14 +1,14 @@
-import { reactive } from "vue";
-import { type User } from "./pocketbase";
+import { reactive } from 'vue'
+import { type User } from './pocketbase'
 
 interface UserStore {
-	user: User | null;
-	setUser: (newUser: User | null) => void;
+  user: User | null
+  setUser: (newUser: User | null) => void
 }
 
 export const userStore = reactive<UserStore>({
-	user: null,
-	setUser(newUser) {
-		this.user = newUser;
-	}
-});
+  user: null,
+  setUser(newUser) {
+    this.user = newUser
+  },
+})
